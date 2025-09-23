0ecdf1982c637ca2caa8ac463882757f365dfe7e
26ahe3 <ahe3>
2025-09-22 23:30:12 -0400

    added some psuedocode

--- disk.cc
+++ disk.cc
@@ -12,7 +12,8 @@ using namespace std;
 std::unordered_map<int, int> diskQueue;   //requester id and track
 int num_alive_requesters = 0; //threads alive
 int currentDiskPos = 0; //disk head position
-int max_disk_queue = 0;
+int max_disk_queue = 0; // will be passed in as first argument in command line 
+
 
 //locks and CVs
 const unsigned int queue_lock = 1;
@@ -25,33 +26,50 @@ struct Requester {
     std::ifstream file; //ensures each disk is only reading from its own track 
 };
 
+// Global variables
 std::vector<Requester> requesters;
+std::queue<int> requestsQueue;
 
-void createThread(void* arg) {
-    num_alive_requesters = requesters.size();
+void createThreads(void* arg) {
+    num_alive_requesters = requesters.size(); // seems problematic
 
     //create a thread per requester
     for(auto &r: requesters) {
         //checks to see if the thread is actually created
-        if(thread_create(thread_startfunc_t) Request, &r) {
-            exit(1);
-        }
+        thread_create((thread_startfunc_t) Request, &r);
     }
+    
+    thread_create((thread_startfunc_t) Service, nullptr);
 
-    if(thread_create(thread_startfunc_t), Service, nullptr) {
-        exit(1);
-    }
 }
+    // Old implementation (commeneted out bc i don't think that is how thread_create works)
+        //     if(thread_create(thread_startfunc_t) Request, &r) {
+    //         exit(1);
+    //     }
+    // }
 
-// void Request(void* arg) {
+    // if(thread_create(thread_startfunc_t), Service, nullptr) {
+    //     exit(1);
+//}
+
+void Request(void* arg) {
     //Will need to cast the pointer back so the thread knows its own requester id and file
     //This is because ThreadCreate requires void*
-    //Requester* r = (Requester*) arg;
-// }
+    Requester* r = (Requester*) arg;
 
-// void Service(void* arg) {
+    /*
+    lock
+    for (line in r->file) {
+        add line to requestsQueue;
+        ...
+    }  
+    unlock
+    */
+}
 
-// }
+void Service(void* arg) {
+
+}
 
 int main(int argc, char* argv[]) {
     //ensures proper amount of command line arguments
@@ -67,7 +85,7 @@ int main(int argc, char* argv[]) {
     //build the requesters
     //output is a vector of each requester id and the open file
     for(int i = 2; i < argc; i++) {
-        Requester r;
+        Requester r; // made this ri so that the name of the Requester would change
         r.id = i - 2; //first disc will start at 0
         r.file.open(argv[i]);
         if(!r.file.is_open()) {
